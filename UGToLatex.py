bce3ae311fcd210c13cccc6eaf199d221febc3e9
Kaiming  Zhang <kz3640@cs.rit.edu>
2022-04-05 13:27:01 -0400

    Added comments

--- UGToLatex.py
+++ UGToLatex.py
@@ -2,72 +2,115 @@ import constants
 import sys
 
 def main(name, inp, out):
-    lines = []
-    w = open(out, "a")
-    w.write(constants.HEADER)
-    with open(inp) as f:
-        for l in f:
-            lines.append(l)
-    lines = rmJunk(lines)
-    i = 0
-    while i != len(lines):
-        if whatIsThis(lines[i]) == 2:
-            if (i != len(lines)-1):
-                if whatIsThis(lines[i+1] == 3)
-                    print(combine(lines[i] , lines[i+1]))
-                    w.write(combine(lines[i], lines[i+1]))
-                    w.write("\n")
-        i+=1
-    w.write(constants.FOOTER)
+    """
+    The main method
+
+    name    The name of the song
+    inp     The name of the text file containing the song
+    out     The name of the text file to output the song
+    """
+
+    writer = open(out, "a")         #the writer to the output
+    reader = open(inp)              #the reader to the input
+    lines = []                      #the array of lines in the input
+
+    writer.write(constants.HEADER)  #write the tex file header
+
+    for line in reader:
+        lines.append(line)          #place all lines in a list
+
+    lines = rmJunk(lines)           #remove junk lines
+
+    i = 0                           #loop counter
+    while i != len(lines):          #loop through all lines
+        if (whatIsThis(lines[i]) == 2 and
+        i != len(lines) -1 and  
+        whatIsThis(lines[i+1] == 3)):
+        #this code runs if the current line is chords, and the next line
+        #is lyrics
+            writer.write(combine(lines[i], lines[i+1]))
+            writer.write("\n")
+            i+=1                    #advance loop an extra time
+        i+=1                        #advance loop
+    writer.write(constants.FOOTER)  #write the tex file footer
     return
 
 def rmJunk(lines):
-    returnMe = []
-    check = ["|", "---"]
-    i = 0
-    while i != len(lines):
+    """
+    Removes unneeded lines
+    lines   The lines to search through
+    """
+
+    returnMe = []                   #the cleaned lines
+    check = ["|", "---"]            #what to look for in an unneeded line
+    i = 0                           #loop counter
+
+    while i != len(lines):          #loop through all lines
         if any(ext in lines[i] for ext in check):
             print("removed line " + str(i) + ".")
         else:
             returnMe.append(lines[i])
-        i+=1
+        i+=1                        #advance loop
     return returnMe
 
 def combine(chords, lyrics):
-    combined = lyrics
-    chords = chords + ' '
-    i = len(chords)
-    while i != 0:
-        if chords[i-1:i] == 'b':
+    """
+    Combines chords and lyrics
+    chords  The chords to be combined
+    lyrics  The lyrics to be combined
+    """
+
+    combined = lyrics               #set the eventual return to lyrics
+    chords = chords + ' '           #add a space to the end of chords
+                                    #to search for eventually
+
+    s = "\\["                       #enclose chord in tex
+    e = "]"
+
+    i = len(chords)                 #loop counter
+    while i != 0:                   #loop thorugh all indexes, backwards
+        if chords[i-1:i] == 'b':    #change flats
             chords = chords[0:i-1] + constants.FLAT + chords[i:]
-        if chords[i-1:i] == '\#':
+        elif chords[i-1:i] == '\#': #change sharps
             chords = chords[0:i-1] + constants.SHARP + chords[i:]
-        if (chords[i-1:i] == 'A' or
-            chords[i-1:i] == 'B' or
-            chords[i-1:i] == 'C' or
-            chords[i-1:i] == 'D' or
-            chords[i-1:i] == 'E' or
-            chords[i-1:i] == 'F' or
-            chords[i-1:i] == 'G'):
-            j = chords.index(' ', i)
-            combined = lyrics[0:i-1] + "\\[" + chords[i-1:j] + "]" + lyrics[i-1:]
+        elif (chords[i-1:i] == 'A' or
+              chords[i-1:i] == 'B' or
+              chords[i-1:i] == 'C' or
+              chords[i-1:i] == 'D' or
+              chords[i-1:i] == 'E' or
+              chords[i-1:i] == 'F' or
+              chords[i-1:i] == 'G'):
+
+            j = chords.index(' ', i)#look for the first occurrence of
+                                    #a space after the chord
+            combined = lyrics[0:i-1] + s + chords[i-1:j] + e + lyrics[i-1:]
         i-=1
     return combined
 
 def whatIsThis(line):
+    """
+    Tells whether a line is a chord, lyrics, verse header, or blank.
+    line    The line to look at
+    """
     #-1: unknown
     # 0: empty line
     # 1: verse header
     # 2: chords
     # 3: lyrics
-    if line[0:1] == "[": #like in [verse 1], [chorus], etc
+    if line[0:1] == "[":            #like in [verse 1], [chorus], etc
         return 1
-    if line.count(' ') == len(line): #empty line
+    if line.count(' ') == len(line):#empty line
         return 0
-    if line.count(' ') > len(line)/3: #space ocurrs more than 1 in 3 chars
+    if line.count(' ') > len(line)/3:
         return 2
-    if len(line) < 4:  #only 1/2/3 char on a line (lone chord)
+    if len(line) < 4:               #only 1/2/3 char on a line
         return 2
+    if line.count('a') +
+       line.count('e') +
+       line.count('i') +
+       line.count('o') +
+       line.count('u') == 0:        #no vowels
+        return 2                    
     return 3
 
 if __name__ == "__main__":
